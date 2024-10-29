c61315e1744b150727adb0f04edca76c8c708e02
AGuedes <115166939+GuedesAlexandre@users.noreply.github.com>
2024-10-29 11:23:11 +0100

    feat(102): init of code for backend application

---
+++ src/main/java/org/mmi/MMIPlatform/MmiPlatformApplication.java
@@ -0,0 +1,13 @@
+package org.mmi.MMIPlatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmiPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MmiPlatformApplication.class, args);
+	}
+
+}
