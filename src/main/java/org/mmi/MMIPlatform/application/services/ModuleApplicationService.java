48d50d870b6298bf8b0fe706ea09d1860af2ecf2
AGuedes <115166939+GuedesAlexandre@users.noreply.github.com>
2024-11-28 16:31:19 +0100

    feat(--noticket) : getAllModules

---
+++ src/main/java/org/mmi/MMIPlatform/application/services/ModuleApplicationService.java
@@ -0,0 +1,42 @@
+package org.mmi.MMIPlatform.application.services;
+
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.mmi.MMIPlatform.application.dto.ModuleDto;
+import org.mmi.MMIPlatform.application.dto.StudentDto;
+import org.mmi.MMIPlatform.domain.mapper.ModuleDtoMapper;
+import org.mmi.MMIPlatform.domain.services.ModuleDomainService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class ModuleApplicationService {
+
+    private final ModuleDomainService moduleDomainService;
+    private final ModuleDtoMapper moduleDtoMapper;
+
+    @Operation(summary = "get all modules", security = @SecurityRequirement(name = "bearerAuth"))
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "modules recover",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ModuleDto.class))}),
+    })
+    public List<ModuleDto> getAllModules() {
+        try {
+            return moduleDtoMapper.moduleListToModuleDtoList(this.moduleDomainService.getAllModules());
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return null;
+        }
+    }
+}
