0c4c8b41546b000b0ecc947c9db22cbbb22af44e
lenormandmorgane <morg.l.n1001@gmail.com>
2025-01-15 11:11:55 +0100

    feat: init Internship tab in infra

---
+++ src/main/java/org/mmi/MMIPlatform/infrastructure/dao/InternshipDao.java
@@ -0,0 +1,39 @@
+package org.mmi.MMIPlatform.infrastructure.dao;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.mmi.MMIPlatform.infrastructure.dao.enums.TypeEnum;
+
+@Entity
+@AllArgsConstructor
+@Builder
+@NoArgsConstructor
+@Data
+@Getter
+@Setter
+@Table(name="MMIPLATFORM_INTERNSHIP")
+public class InternshipDao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "MMI_PLATFORM_INTERNSHIP_ID")
+    private String id;
+
+    @Column(name = "MMI_PLATFORM_INTERNSHIP_TITLE")
+    private String title;
+
+    @Column(name = "MMI_PLATFORM_INTERNSHIP_COMMENT")
+    private String comment;
+
+    @Column(name = "MMI_PLATFORM_INTERNSHIP_WEEKNUMBER")
+    private String weekNumber;
+
+    @Column(name = "MMI_PLATFORM_INTERNSHIP_TYPE")
+    @Enumerated(EnumType.STRING)
+    private TypeEnum type;
+
+    @ManyToOne
+    @JoinColumn(name = "MMI_PLATFORM_STUDENT_ID")
+    @ToString.Exclude
+    private StudentDao student;
+}
---
+++ src/main/java/org/mmi/MMIPlatform/infrastructure/dao/enums/TypeEnum.java
@@ -0,0 +1,11 @@
+package org.mmi.MMIPlatform.infrastructure.dao.enums;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public enum TypeEnum {
+    ALTERNANCE("ALTERNANCE"),
+    STAGE("STAGE");
+
+    private final String TypeEnum;
+}
