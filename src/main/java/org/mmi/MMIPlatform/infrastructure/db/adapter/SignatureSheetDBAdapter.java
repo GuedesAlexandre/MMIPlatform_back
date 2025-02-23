a7dce355b13025f89a19d90b521cd7a9490949b3
AGuedes <115166939+GuedesAlexandre@users.noreply.github.com>
2025-02-23 10:17:33 +0100

    fix() : try parsing format

--- src/main/java/org/mmi/MMIPlatform/infrastructure/db/adapter/SignatureSheetDBAdapter.java
+++ src/main/java/org/mmi/MMIPlatform/infrastructure/db/adapter/SignatureSheetDBAdapter.java
@@ -13,6 +13,11 @@ import org.springframework.stereotype.Service;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.Date;
 import java.util.List;
 
@@ -56,16 +61,29 @@ public class SignatureSheetDBAdapter {
         }
     }
 
-    public SignatureSheetDao getSignatureSheetByModuleNameAndPromoAndCreatedAtAndFinishAt(String moduleName, String promo, String createdAt, String finishAt) throws Exception {
+    public SignatureSheetDao getSignatureSheetByModuleNameAndPromoAndCreatedAtAndFinishAt(
+            String moduleName, String promo, String createdAt, String finishAt) throws Exception {
         try {
-            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS XXX");
-            return this.signatureSheetDaoRepository.findByModuleNameAndPromoAndCreatedAtAndFinishAt(moduleName, PromoEnum.valueOf(promo), dateFormat.parse(createdAt), dateFormat.parse(finishAt));
-        } catch (ParseException e) {
+            createdAt = createdAt.replace(" ", "+");
+            finishAt = finishAt.replace(" ", "+");
+
+            DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
+
+            OffsetDateTime createdAtDateTime = OffsetDateTime.parse(createdAt, dateTimeFormatter);
+            Date createdAtDate = Date.from(createdAtDateTime.toInstant());
+
+            OffsetDateTime finishAtDateTime = OffsetDateTime.parse(finishAt, dateTimeFormatter);
+            Date finishAtDate = Date.from(finishAtDateTime.toInstant());
+
+            return this.signatureSheetDaoRepository.findByModuleNameAndPromoAndCreatedAtAndFinishAt(
+                    moduleName, PromoEnum.valueOf(promo), createdAtDate, finishAtDate);
+        } catch (DateTimeParseException e) {
             throw new Exception("Failed to parse date: " + e.getMessage(), e);
         } catch (Exception e) {
             throw new Exception("An error occurred: " + e.getMessage(), e);
         }
     }
+
 
     public SignatureSheetDao saveSignatureSheet(SignatureSheetDao signatureSheetDao) throws Exception {
         try {
